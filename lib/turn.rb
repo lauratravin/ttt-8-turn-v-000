8a156a0c9d6a4bb8b32cbdf9dfae8b529ebf19ac
Laura Messa Travin <laura.travin@gmail.com>
2018-01-27 02:25:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,37 +6,37 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-end
-
-def valid_move?(board, position)
-  if position.between?(0,8) && board[position] == " "
-    true
-  else
-    false
-  end
-end
-
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-end
-
-def move(board, index, value="X")
-  board[index] = value
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-
-def position_taken?(board, position)
-  if board[position] == "X" || board[position] == "O"
-    true
-  elsif board[position] == " "
-    false
-  else
-    false
-end
+# def input_to_index(user_input)
+#   index = user_input.to_i - 1
+# end
+#
+# def valid_move?(board, position)
+#   if position.between?(0,8) && board[position] == " "
+#     true
+#   else
+#     false
+#   end
+# end
+#
+# def input_to_index(user_input)
+#   index = user_input.to_i - 1
+# end
+#
+# def move(board, index, value="X")
+#   board[index] = value
+#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#   puts "-----------"
+#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+#   puts "-----------"
+#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# end
+#
+#
+# def position_taken?(board, position)
+#   if board[position] == "X" || board[position] == "O"
+#     true
+#   elsif board[position] == " "
+#     false
+#   else
+#     false
+# end
