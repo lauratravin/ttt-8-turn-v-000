951261f1005951f2be6ea5537f3ce5dff1a5b400
Laura Messa Travin <laura.travin@gmail.com>
2018-01-27 02:37:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,19 +22,16 @@ def valid_move?(board, position)
   end
 end
 
-# def input_to_index(user_input)
-#   index = user_input.to_i - 1
-# end
-#
-# def move(board, index, value="X")
-#   board[index] = value
-#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-#   puts "-----------"
-#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-#   puts "-----------"
-#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# end
-#
+
+def move(board, index, value="X")
+  board[index] = value
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 #
 # def position_taken?(board, position)
 #   if board[position] == "X" || board[position] == "O"
